764d8040dd9384ebd2cb41a29b661f9039c365b2
lixinduan <lixinduan@tencent.com>
2021-07-05 17:17:28 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@
 
   <button type="button" @click="state.count++">count is: {{ state.count }}</button>
   <p>
-    Edit
+    Edit111
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
