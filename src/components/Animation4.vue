ad97025c778815594e30b33dd1fbcadb1fe8f42a
Yihan <123@163.com>
2021-08-02 13:32:09 +0800

    First commit

---
+++ src/components/Animation4.vue
@@ -0,0 +1,94 @@
+<template>
+    <h2 class="title">
+        动画4-从小变大然后消失
+    </h2>
+    <div class="content">
+        <div class="animation-wrapper">
+            <div class="box disapear">
+                变大消失
+            </div>
+        </div>
+        <button class="btn" @click="bigger">
+            开始动画
+        </button>
+    </div>
+</template>
+
+<script>
+import gsap from 'gsap';
+
+export default {
+    name: 'Animation4',
+    data(){
+        return{
+        };
+    },
+    methods:{
+        bigger(){
+            const tl = gsap.timeline();
+            tl.to('.disapear', {duration:1, scale:2, opacity:0});
+        },
+    },
+};
+</script>
+
+<style scoped>
+.title {
+  margin-top: 50px;
+  margin-left: 20px;
+}
+.content {
+  margin-top: 30px;
+  margin-left: 20px;
+}
+.code {
+  margin: 20px;
+}
+.animation-wrapper {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 500px;
+  height: 500px;
+
+  overflow: hidden;
+  border: 2px solid red;
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+
+  border: 2px solid green;
+}
+.middle {
+  width: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  border: none;
+}
+.left-to-right {
+  position: absolute;
+  height: 200px;
+  width: 500px;
+  box-sizing: border-box;
+  left: -500px;
+}
+.fly-top {
+  position: absolute;
+  box-sizing: border-box;
+  top: 500px;
+}
+.hide-to-show {
+  opacity: 0;
+}
+.zoom-small-fly {
+  /* position: absolute; */
+}
+</style>
---
+++ src/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <swiper
+    :slides-per-view="3"
+    :space-between="50"
+    navigation
+    :pagination="{ clickable: true }"
+    :scrollbar="{ draggable: true }"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide><img src="../assets/1.jpg" alt="image1"></swiper-slide>
+    <swiper-slide><img src="../assets/2.jpg" alt="image2"></swiper-slide>
+    <swiper-slide><img src="../assets/3.jpg" alt="image3"></swiper-slide>
+    ...
+  </swiper>
+</template>
+<script>
+  // import Swiper core and required modules
+  import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/swiper.scss';
+  import 'swiper/components/navigation/navigation.scss';
+  import 'swiper/components/pagination/pagination.scss';
+  import 'swiper/components/scrollbar/scrollbar.scss';
+
+  // install Swiper modules
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+  // Import Swiper styles
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    methods: {
+      onSwiper(swiper) {
+        console.log(swiper);
+      },
+      onSlideChange() {
+        console.log('slide change');
+      },
+    },
+  };
+</script>
